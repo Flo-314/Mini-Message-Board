6710a1c32bf84eb4b557c4ac44036cc946cab3d8
Flo-314 <julianflo1916@gmail.com>
2022-01-07 20:25:00 -0300

    post y get  puestos

--- msg-app/routes/index.js
+++ msg-app/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    date: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    date: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages });
 });
+// Post From new
+router.post("/new", (req,res,next) =>{
+    
+  
+  const newPost = {
+    title: req.body.title,  
+    text: req.body.msg,
+    user: req.body.username,
+    date: new Date(),
+    }
+    messages.push(newPost)
+  res.redirect('/')
+})
+
 
 module.exports = router;
--- msg-app/routes/newForm.js
+++ msg-app/routes/newForm.js
@@ -6,4 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('form', { title: 'Express' });
 });
 
+
 module.exports = router;
